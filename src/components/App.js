3311287cbc4516398d78648e3393d097e0ce6445
Ryosuke Hiroe <ride.poke@gmail.com>
2021-04-05 13:53:26 +0900

    tailwindcss関連の記述を削除

--- src/components/App.js
+++ src/components/App.js
@@ -14,23 +14,25 @@ class Sample extends React.Component {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Sample text={'ボタンだよ'}/>
-    </div>
+    <header className="">
+    </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <Sample text={'ボタンだよ'}/>
+    // </div>
   );
 }
 
